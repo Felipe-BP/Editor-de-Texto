3b687c74e2a194ffd905fb258c111e6b4bdac477
Felipe Bueno <gvv.fel@gmail.com>
2018-04-19 15:13:41 -0300

    Terceira Atualização do Sistema

--- src/View/Interface.java
+++ src/View/Interface.java
@@ -6,12 +6,16 @@
 package view;
 import javax.swing.*;
 import java.awt.*;
-import static java.awt.Frame.MAXIMIZED_BOTH;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.border.Border;
 /**
  *
@@ -23,10 +27,14 @@ public class Interface{
     private static final Dimension DIMENSION = new Dimension(1000, 700);
     //dimension para o panel
     private static final Dimension DIMENSIONP = new Dimension(50, 50);
+    //dimension para a coluna
+    private static final Dimension DIMENSIONC = new Dimension(35, 50);
     //borda
     private static final Border BORDA = BorderFactory.createLineBorder(Color.black);
     //ponto de click inicial
     private Point clickInicial;
+    //numeros para barra
+    private static int i = 2;
     
     Interface(){
        //defininção do frame principal
@@ -67,11 +75,13 @@ public class Interface{
             }
         });
         
+        //criação do panel para conter icones e menu
         JPanel panel = new JPanel();
         panel.setBorder(BORDA);
         panel.setPreferredSize(DIMENSIONP);
         panel.setLayout(new GridLayout(2, 0));
         
+        //paineis com icones e suas ações, juntamente com o painel da barra de menu
         JPanel subpanel1 = new JPanel();
         subpanel1.setBorder(BORDA);
         subpanel1.setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -94,12 +104,16 @@ public class Interface{
                 frame.setLocation(X, Y);
             }
         });
-        Icon fecharimg = new ImageIcon("C:\\Users\\Rafael\\Pictures\\icons8-cancelar-filled-12.png");
-        Icon aumentarimg = new ImageIcon("C:\\Users\\Rafael\\Pictures\\icons8-redimensionar-diagonal-filled-10.png");
-        JLabel sublabel1 = new JLabel(fecharimg);
-        JLabel sublabel2 = new JLabel(aumentarimg);
+
+        JLabel sublabel1 = new JLabel();
+        sublabel1.setIcon(new ImageIcon(setImage("icons\\icons8-cancelar-filled-12.png")));
+        JLabel sublabel2 = new JLabel();
+        sublabel2.setIcon(new ImageIcon(setImage("icons\\icons8-redimensionar-diagonal-filled-10.png")));
+        JLabel sublabel3 = new JLabel();
+        sublabel3.setIcon(new ImageIcon(setImage("icons\\icons8-comprimir-filled-10.png")));
         subpanel1.add(sublabel1);
         subpanel1.add(sublabel2);
+        subpanel1.add(sublabel3);
         sublabel1.addMouseListener(new MouseAdapter(){
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -110,7 +124,13 @@ public class Interface{
         sublabel2.addMouseListener(new MouseAdapter(){
             @Override
             public void mouseClicked(MouseEvent e) {
-                frame.setExtendedState(MAXIMIZED_BOTH);
+                frame.setExtendedState(frame.getExtendedState() == frame.MAXIMIZED_BOTH ? frame.NORMAL : frame.MAXIMIZED_BOTH);
+            }
+        });
+        sublabel3.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                frame.setExtendedState(JFrame.ICONIFIED);
             }
         });
         JPanel subpanel2 = new JPanel();
@@ -118,17 +138,44 @@ public class Interface{
         subpanel2.setLayout(new GridLayout(1, 0));
         subpanel2.add(barraMenu);
         
+        //adicionando os subpaineis ao painel
         panel.add(subpanel1);
         panel.add(subpanel2);
         
+        //coluna para numero de linhas
+        JPanel coluna = new JPanel();
+        coluna.setBorder(BORDA);
+        coluna.setLayout(new BorderLayout());
+        coluna.setPreferredSize(DIMENSIONC);
+        JLabel um = new JLabel("1");
+        coluna.add(um, BorderLayout.NORTH);
+        JPanel subpanelnum = new JPanel();
+        subpanelnum.setLayout(new FlowLayout());
+        coluna.add(subpanelnum);
+        
+        //criação do TextArea 
         JTextArea txtArea = new JTextArea();
         txtArea.setBorder(BORDA);
+        txtArea.addKeyListener(new KeyAdapter(){
+            public void keyPressed(KeyEvent e){
+                if(e.getKeyCode() == KeyEvent.VK_ENTER){
+                    JTextField numeros = new JTextField(i+"\n");
+                    numeros.setEditable(false);
+                    subpanelnum.add(numeros);
+                    i++;
+                }
+            }
+        });
         
+        //adicionando os componentes ao frame principal
         frame.add(panel, BorderLayout.NORTH);
+        frame.add(coluna, BorderLayout.WEST);
         frame.add(txtArea);
         
+        //empacotando 
         frame.pack();
         
+        //deixando o frame principal visivel
         frame.setVisible(true);        
     }
     
@@ -145,5 +192,14 @@ public class Interface{
         if(i==0){
             System.exit(0);
         }
+    }
+    
+    private BufferedImage setImage(String path){
+        try{
+            return ImageIO.read(getClass().getClassLoader().getResource(path));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
